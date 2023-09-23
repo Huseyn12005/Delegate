d5b402aed82874fc1f81ba16675185347ce63187
Huseyn <horucov148@gmail.com>
2023-09-23 21:08:53 +0400

    MyClass_Delegate

--- Delegate/MyClass.cs
+++ Delegate/MyClass.cs
@@ -1,6 +1,32 @@
 ﻿namespace Delegate
 {
+    delegate void Func(string str);
     internal class MyClass
     {
+        public string Str { get; set; }
+        public MyClass(string str) { Str = str; }
+        public void Space(string Str)
+        {
+
+            string result = string.Join("_", Str.ToCharArray());
+            Console.WriteLine(result);
+        }
+
+        public void Reverse(string Str)
+        {
+            char[] charArray = Str.ToCharArray();
+            Array.Reverse(charArray);
+            string result = new string(charArray);
+            Console.WriteLine(result);
+        }
+    }
+
+    internal class Run
+    {
+        public void runFunc(Func func, string str)
+        {
+
+            func(str);
+        }
     }
 }
--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using Delegate;
+
+Console.WriteLine("Enter string");
+
+var str = Console.ReadLine();
+MyClass cls = new MyClass(str!);
+Func funcDell = cls.Space;
+funcDell += cls.Reverse;
+Run run = new Run();
+run.runFunc(funcDell, str!);
